e53033637961c19d1d33797842493a9cbbca2027
Johnny Rice <jrice@labact.com>
2020-07-02 23:13:30 -0400

    Provide a function writing challenge for Erick and his cohort crew

---
+++ challenge04.js
@@ -0,0 +1,26 @@
+
+const puppers = [{
+    age: 2,
+    name: 'Ubu'
+}, {
+    age: 7,
+    name: 'DJ Yella'
+}]
+
+// 1) complete this function that will display the name and age of each pup
+function () {
+
+}
+
+// 2) call the new function
+
+
+// 3) Got all that working? 💪 Now make a copy of the function and this time
+// 3a) give it a new name 
+// 3b) make the new version an arrow function
+
+
+// 4) call the new arrow function
+
+
+
